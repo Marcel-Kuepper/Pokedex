f3df7436a4f307350bff3d9951f88fdb19750a76
Marcel Kuepper <marcelk95@hotmail.de>
2022-11-21 14:41:49 +0100

    design changed

--- script.js
+++ script.js
@@ -1,38 +1,46 @@
 let currentPokemon = ``;
 let allPokemon = ``;
+let selectedPokemon = ``;
 
 
 async function showPokemonList() {
-    let url = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=905`;
-    let response = await fetch(url);
-    let allPokemons = await response.json();
-    allPokemon = await allPokemons['results'];
+    for (let i = 1; i < 906; i++) {
+        let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        let response = await fetch(url);
+        currentPokemon = await response.json();
+        renderPokedexList(i)
+    }
 
-    console.log(allPokemon);
-    renderPokedexList()
 }
 
-function renderPokedexList() {
-    document.getElementById('pokemon-list').innerHTML = '';
-    for (let i = 0; i < allPokemon.length; i++) {
-        const pokemon = allPokemon[i];
-        document.getElementById('pokemon-list').innerHTML += `<div class="select" onclick="showPokemon('${pokemon['name']}')">#${i+1} ${pokemon['name']}</div>`;
+function renderPokedexList(i) {
+    document.getElementById('pokemon-list').innerHTML += `
+        <div class="select bg${currentPokemon["types"]["0"]["type"]["name"]}" onclick="showPokemon('${currentPokemon['name']}')">
+        <div class="pokemon-info">
+        <div class="pokename"><b>#${i} ${currentPokemon["name"].charAt(0).toUpperCase() + currentPokemon["name"].slice(1)}</b></div>
+        <div id="types${i}" class="types"><div class="${currentPokemon["types"]["0"]["type"]["name"]}">${currentPokemon["types"]["0"]["type"]["name"]}</div></div>
+        </div>
+        <img src="${currentPokemon['sprites']['other']['official-artwork']['front_default']}">
+        </div>`;
+    if (currentPokemon['types'].length > 1) {
+        document.getElementById(`types${i}`).innerHTML += `<div class="${currentPokemon["types"]["1"]["type"]["name"]}">${currentPokemon["types"]["1"]["type"]["name"]}</div>`
     }
 }
 
 async function showPokemon(pokemon) {
     let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
     let response = await fetch(url);
-    currentPokemon = await response.json();
+    selectedPokemon = await response.json();
 
     renderPokedex();
 }
 
 function renderPokedex() {
-    document.getElementById(`pokemon-name`).innerHTML = currentPokemon[`name`];
+    document.getElementById(`pokemon-name`).innerHTML = selectedPokemon[`name`];
+    document.getElementById(`pokemon-img`).innerHTML = `<img src="${selectedPokemon['sprites']['other']['official-artwork']['front_default']}">`
     document.getElementById('abilitys').innerHTML = '';
-    for (let i = 0; i < currentPokemon['abilities'].length; i++) {
-        const ability = currentPokemon['abilities'][i];
+    for (let i = 0; i < selectedPokemon['abilities'].length; i++) {
+        const ability = selectedPokemon['abilities'][i];
         document.getElementById('abilitys').innerHTML += `<div>${ability['ability']['name']}</div>`
     }
 }
